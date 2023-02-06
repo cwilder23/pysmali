4c9517a04970e9a7694ac04d59870ad6fcc591de
Christopher Wilder <christopher.wilder@perspectalabs.com>
2023-02-06 11:36:08 -0500

    Cleaned up code

--- smali/__init__.py
+++ smali/__init__.py
@@ -4,16 +4,12 @@ import sys
 if sys.hexversion < 0x03080000:
     raise Exception('python 3.8 or newer required')
 
-# cwilder (12/20/2022) - Ignoring missing VERSION file exception b/c it doesn't matter. TODO: make permanent solution; a.k.a. include the VERSION file
-# cwilder (12/20/2022) - Adding an empty VERSION file as an additional/alt. solution.
-# Also see 'https://github.com/roytu/pysmali/blob/master/smali/__init__.py' for simple fix
-#try:
-#    with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION'), 'r') as f:
-#        __version__ = f.read()
-#except Exception as ex:
-#    __version__ = "0.2.5"
-#    #print(f'Warning: Missing "{os.path.join(os.path.dirname(__file__), '..', 'VERSION')}"', ex)
-__version__ = "0.2.5"
+try:
+    with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION'), 'r') as f:
+        __version__ = f.read()
+except Exception as ex:
+    __version__ = "0.2.5"
+    print(f"Warning: Missing '{os.path.join(os.path.dirname(__file__), '..', 'VERSION')}", ex)
 
 from smali.smali_file import SmaliFile
 
