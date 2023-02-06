59060da998a52f3e571fa008dba8198f76838804
Christopher Wilder <christopher.wilder@perspectalabs.com>
2023-02-06 11:49:52 -0500

    Seriously simplified fix. Don't know why I did anything different

--- smali/__init__.py
+++ smali/__init__.py
@@ -4,12 +4,7 @@ import sys
 if sys.hexversion < 0x03080000:
     raise Exception('python 3.8 or newer required')
 
-try:
-    with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION'), 'r') as f:
-        __version__ = f.read()
-except Exception as ex:
-    __version__ = "0.2.5"
-    print(f"Warning: Missing {os.path.join(os.path.dirname(__file__), '..', 'VERSION')}", ex)
+__version__ = "0.2.5"
 
 from smali.smali_file import SmaliFile
 
